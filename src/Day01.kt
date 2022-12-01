5b10de7d3d1c37f99c7f06ac5b9ead2817cb9d3c
Krystian Kaniowski <kanialdo@gmail.com>
2022-12-01 22:32:01 +0100

    Add Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun part1(input: String): Int {
+        val elves = input.parse()
+        return elves.max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        val elves = input.parse()
+        return elves.findTopNElves(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInputAsText("Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readInputAsText("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+private fun String.parse(): List<Int> = split("\n\n").map { calories ->
+    calories.lines().sumOf { it.toInt() }
+}
+
+private fun List<Int>.findTopNElves(n: Int): List<Int> {
+    check(n < this.size)
+    return this.sortedDescending()
+        .take(n)
 }
