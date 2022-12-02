f77fe0ee895fcaab7202f5b7a591b41022e47cc2
Krystian Kaniowski <kanialdo@gmail.com>
2022-12-02 15:04:54 +0100

    Add Day02 shorter solution

--- src/Day02.kt
+++ src/Day02.kt
@@ -2,24 +2,20 @@ fun main() {
 
     fun part1(input: String): Int {
         val rounds = input.lines()
-        var score = 0
-        rounds.forEach { round ->
+        return rounds.sumOf { round ->
             val left = round[0].code - 'A'.code
             val right = round[2].code - 'X'.code
-            score += (right - left + 4) % 3 * 3 + right % 3 + 1
+            (right - left + 4) % 3 * 3 + right % 3 + 1
         }
-        return score
     }
 
     fun part2(input: String): Int {
         val rounds = input.lines()
-        var score = 0
-        rounds.forEach { round ->
+        return rounds.sumOf { round ->
             val left = round[0].code - 'A'.code
             val right = round[2].code - 'X'.code
-            score += (left + right + 2) % 3 + 1 + right * 3
+            (left + right + 2) % 3 + 1 + right * 3
         }
-        return score
     }
 
     val testInput = readInputAsText("Day02_test")
