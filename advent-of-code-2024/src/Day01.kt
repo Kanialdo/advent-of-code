1c5a7ab98b7abffb1a1cf8bfb4f937650b491f83
Krystian Kaniowski <kanialdo@gmail.com>
2024-12-01 22:41:50 +0100

    aoc-2024 - Day01 improved

--- advent-of-code-2024/src/Day01.kt
+++ advent-of-code-2024/src/Day01.kt
@@ -4,30 +4,49 @@ fun main() {
 
     fun part1(input: List<String>): Int {
 
-        val data = input.map { it.split("   ").map { it.toInt() } }
+        val left = mutableListOf<Int>()
+        val right = mutableListOf<Int>()
 
-        val left = data.map { it[0] }.sorted()
-        val right = data.map { it[1] }.sorted()
+        input.forEach {
+            it.split("   ")
+                .map { it.toInt() }
+                .let {
+                    left.add(it[0])
+                    right.add(it[1])
+                }
+        }
+
+        left.sort()
+        right.sort()
 
         return left.mapIndexed { index, value -> abs(value - right[index]) }.sum()
     }
 
     fun part2(input: List<String>): Int {
 
-        val data = input.map { it.split("   ").map { it.toInt() } }
+        val left = mutableListOf<Int>()
+        val right = mutableListOf<Int>()
 
-        val left = data.map { it[0] }.sorted()
-        val right = data.map { it[1] }.sorted()
+        input.forEach {
+            it.split("   ")
+                .map { it.toInt() }
+                .let {
+                    left.add(it[0])
+                    right.add(it[1])
+                }
+        }
 
-        return left.sumOf { leftNumber -> leftNumber * right.count { rightNumber -> leftNumber == rightNumber } }
+        val right2 = right.groupBy { it }
+
+        return left.sumOf { leftNumber -> leftNumber * (right2[leftNumber]?.size ?: 0) }
     }
 
     val testInput = readInput("Day01_test")
     val input = readInput("Day01")
 
-    check(part1(testInput) == 11)
-    part1(input).println()
+    test(11) { part1(testInput) }
+    exec { part1(input) }
 
-    check(part2(testInput) == 31)
-    part2(input).println()
+    test(31) { part2(testInput) }
+    exec { part2(input) }
 }
--- advent-of-code-2024/src/Utils.kt
+++ advent-of-code-2024/src/Utils.kt
@@ -2,6 +2,7 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readText
+import kotlin.time.measureTime
 
 /**
  * Reads lines from the given input txt file.
@@ -19,3 +20,26 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+
+fun test(expected: Int, command: () -> Int) {
+    val result: Int
+    val time = measureTime {
+        result = command()
+    }
+
+    if (expected == result) {
+        println("Test passed in ${time.inWholeMicroseconds} µs")
+    } else {
+        error("Test failed. Expected $expected, but was $result")
+    }
+}
+
+fun exec(command: () -> Int) {
+    val result: Int
+    val time = measureTime {
+        result = command()
+    }
+
+    println("Result $result in ${time.inWholeMicroseconds} µs")
+}
