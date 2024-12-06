eb4cf130eb13baf567258576a4716f92b786650d
Krystian Kaniowski <kaniowski@smartpatient.eu>
2024-12-06 22:46:12 +0100

    wip

--- advent-of-code-2024/src/Day06.kt
+++ advent-of-code-2024/src/Day06.kt
@@ -11,13 +11,13 @@ sealed class Field {
     data object Guard : Field()
     data object Wall : Field()
     data object NotVisited : Field()
-    data object Visited : Field()
+    data class Visited(val directions: Set<Direction>) : Field()
     data object NotExists : Field()
 }
 
 data class Array2d(val xSize: Int, val ySize: Int) {
 
-    val array = Array(ySize) { Array<Field>(xSize) { Field.NotVisited } }
+    private val array = Array(ySize) { Array<Field>(xSize) { Field.NotVisited } }
 
     operator fun get(x: Int, y: Int) = if (isInBounds(x, y)) array[y][x] else Field.NotExists
     operator fun set(x: Int, y: Int, field: Field) {
@@ -26,7 +26,7 @@ data class Array2d(val xSize: Int, val ySize: Int) {
 
     fun isInBounds(x: Int, y: Int) = x in 0 until xSize && y in 0 until ySize
 
-    fun count(field: Field) = array.sumOf { it.count { it == field } }
+    fun countVisited() = array.sumOf { it.count { it is Field.Visited || it is Field.Guard } }
 
     fun printArray() {
         for (x in array.indices) {
@@ -36,7 +36,7 @@ data class Array2d(val xSize: Int, val ySize: Int) {
                         Field.Guard -> '^'
                         Field.Wall -> '#'
                         Field.NotVisited -> '.'
-                        Field.Visited -> 'X'
+                        is Field.Visited -> 'X'
                         else -> ' '
                     }
                 )
@@ -67,7 +67,7 @@ fun main() {
                         '.' -> Field.NotVisited
                         '^' -> {
                             guardPos = Point(x, y)
-                            Field.Visited
+                            Field.Guard
                         }
 
                         else -> error("Unknown char: $char")
@@ -81,17 +81,68 @@ fun main() {
             while (array[guardPos.x + direction.x, guardPos.y + direction.y] == Field.Wall) {
                 direction = direction.rotate()
             }
-            array[guardPos.x, guardPos.y] = Field.Visited
+            array[guardPos.x, guardPos.y] = Field.Visited(emptySet())
             guardPos = Point(guardPos.x + direction.x, guardPos.y + direction.y)
 
 
         } while (array[guardPos.x, guardPos.y] != Field.NotExists)
 
-        return array.count(Field.Visited)
+        return array.countVisited()
     }
 
     fun part2(input: List<String>): Int {
-        return TODO()
+
+        val array = Array2d(input[0].length, input.size)
+        var guardPos = Point(0, 0)
+        var direction = Direction.UP
+        var count = 0
+
+        input.forEachIndexed { y, list ->
+            list.forEachIndexed { x, char ->
+                array.set(
+                    x = x,
+                    y = y,
+                    field = when (char) {
+                        '#' -> Field.Wall
+                        '.' -> Field.NotVisited
+                        '^' -> {
+                            guardPos = Point(x, y)
+                            Field.Guard
+                        }
+
+                        else -> error("Unknown char: $char")
+                    }
+                )
+            }
+        }
+
+        do {
+            // array.printArray()
+
+            val directions = (array[guardPos.x, guardPos.y] as? Field.Visited)?.directions ?: emptySet()
+            array[guardPos.x, guardPos.y] = Field.Visited(directions + direction)
+
+            while (array[guardPos.x + direction.x, guardPos.y + direction.y] == Field.Wall) {
+                direction = direction.rotate()
+            }
+            guardPos = Point(guardPos.x + direction.x, guardPos.y + direction.y)
+
+            val tempDirection = direction.rotate()
+            var tempGuardPos = guardPos
+            var tempRoute: Field
+            do {
+                tempRoute = array[tempGuardPos.x, tempGuardPos.y]
+                if (tempRoute is Field.Visited && tempRoute.directions.contains(tempDirection)) {
+                    count++
+                    break
+                }
+                tempGuardPos = Point(tempGuardPos.x + tempDirection.x, tempGuardPos.y + tempDirection.y)
+            } while (tempRoute != Field.Wall && tempRoute != Field.NotExists)
+
+
+        } while (array[guardPos.x, guardPos.y] != Field.NotExists)
+
+        return count
     }
 
     val testInput = readInput("Day06_test")
@@ -99,6 +150,6 @@ fun main() {
 
     test(41) { part1(testInput) }
     exec { part1(input) }
-    test(TODO()) { part2(testInput) }
+    test(6) { part2(testInput) }
     exec { part2(input) }
 }
