ad4a395f19ffc01a0d3950e415616a33927013b1
Krystian Kaniowski <kanialdo@gmail.com>
2025-12-02 23:33:27 +0100

    aoc-2025 - setup

---
+++ advent-of-code-2025/src/Template.kt
@@ -0,0 +1,20 @@
+// https://adventofcode.com/2025/day/TODO()
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        return TODO()
+    }
+
+    fun part2(input: List<String>): Int {
+        return TODO()
+    }
+
+    val testInput = readInput("Day${TODO()}_test")
+    val input = readInput("Day${TODO()}")
+
+    test(TODO()) { part1(testInput) }
+    exec { part1(input) }
+    test(TODO()) { part2(testInput) }
+    exec { part2(input) }
+}
---
+++ advent-of-code-2025/src/Utils.kt
@@ -0,0 +1,111 @@
+import java.math.BigInteger
+import java.security.MessageDigest
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+import kotlin.time.measureTime
+
+/**
+ * Reads lines from the given input txt file.
+ */
+fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
+
+/**
+ * Converts string to md5 hash.
+ */
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+    .toString(16)
+    .padStart(32, '0')
+
+/**
+ * The cleaner shorthand for printing output.
+ */
+fun Any?.println() = println(this)
+
+
+fun test(expected: Int, command: () -> Int) {
+    val result: Int
+    val time = measureTime {
+        result = command()
+    }
+
+    if (expected == result) {
+        println("Test passed in ${time.inWholeMicroseconds} µs")
+    } else {
+        error("Test failed. Expected $expected, but was $result")
+    }
+}
+
+fun testLong(expected: Long, command: () -> Long) {
+    val result: Long
+    val time = measureTime {
+        result = command()
+    }
+
+    if (expected == result) {
+        println("Test passed in ${time.inWholeMicroseconds} µs")
+    } else {
+        error("Test failed. Expected $expected, but was $result")
+    }
+}
+
+fun testDouble(expected: Double, command: () -> Double) {
+    val result: Double
+    val time = measureTime {
+        result = command()
+    }
+
+    if (expected == result) {
+        println("Test passed in ${time.inWholeMicroseconds} µs")
+    } else {
+        error("Test failed. Expected $expected, but was $result")
+    }
+}
+
+fun testBigInteger(expected: BigInteger, command: () -> BigInteger) {
+    val result: BigInteger
+    val time = measureTime {
+        result = command()
+    }
+
+    if (expected == result) {
+        println("Test passed in ${time.inWholeMicroseconds} µs")
+    } else {
+        error("Test failed. Expected $expected, but was $result")
+    }
+}
+
+fun exec(command: () -> Int) {
+    val result: Int
+    val time = measureTime {
+        result = command()
+    }
+
+    println("Result $result in ${time.inWholeMicroseconds} µs")
+}
+
+fun execLong(command: () -> Long) {
+    val result: Long
+    val time = measureTime {
+        result = command()
+    }
+
+    println("Result $result in ${time.inWholeMicroseconds} µs")
+}
+
+fun execDouble(command: () -> Double) {
+    val result: Double
+    val time = measureTime {
+        result = command()
+    }
+
+    println("Result $result in ${time.inWholeMicroseconds} µs")
+}
+
+fun execBigInteger(command: () -> BigInteger) {
+    val result: BigInteger
+    val time = measureTime {
+        result = command()
+    }
+
+    println("Result $result in ${time.inWholeMicroseconds} µs")
+}
