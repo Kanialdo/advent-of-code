f848af7580567ed688c0c9ed846cc7da6d5353cc
Krystian Kaniowski <kanialdo@gmail.com>
2025-12-04 23:01:17 +0100

    aoc-2025 - Day02 - part 2

--- advent-of-code-2025/src/Day02.kt
+++ advent-of-code-2025/src/Day02.kt
@@ -18,7 +18,23 @@ fun main() {
     }
 
     fun part2(input: List<String>): Long {
-        TODO()
+
+        return input.sumOf {
+            var sum = 0L
+            val (from, to) = it.split("-").map { it.toLong() }
+            for (i in from..to) {
+                val number = i.toString()
+                sum += when {
+                    number.length >= 2 && number.chunked(1).all { it == number.take(1) } -> i
+                    number.length >= 4 && number.chunked(2).all { it == number.take(2) } -> i
+                    number.length >= 6 && number.chunked(3).all { it == number.take(3) } -> i
+                    number.length >= 8 && number.chunked(4).all { it == number.take(4) } -> i
+                    number.length >= 10 && number.chunked(5).all { it == number.take(5) } -> i
+                    else -> 0
+                }
+            }
+            sum
+        }
     }
 
     val testInput = readInput("Day02_test").first().split(",")
@@ -27,6 +43,6 @@ fun main() {
     testLong(1227775554) { part1(testInput) }
     execLong { part1(input) }
 
-    testLong(TODO()) { part2(testInput) }
+    testLong(4174379265) { part2(testInput) }
     execLong { part2(input) }
 }
