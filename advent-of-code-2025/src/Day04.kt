c47d2e3c8c37004a722665b5d9b75c7fbd64f6cf
Krystian Kaniowski <kanialdo@gmail.com>
2025-12-05 23:47:11 +0100

    aoc-2025 - Day04 - part 2

--- advent-of-code-2025/src/Day04.kt
+++ advent-of-code-2025/src/Day04.kt
@@ -29,7 +29,40 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        TODO()
+
+        var count = 0
+        val minSize = 0
+        val maxSize = input.size - 1
+
+        val currentInput = input.map { it.toMutableList() }.toMutableList()
+        val toBeRemoved = mutableListOf<Pair<Int, Int>>()
+
+        do {
+            toBeRemoved.forEach {
+                currentInput[it.first][it.second] = '.'
+            }
+            toBeRemoved.clear()
+            for (y in currentInput.indices) {
+                for (x in currentInput.first().indices) {
+                    var rollsAround = 0
+                    if (currentInput[y][x] == '@') {
+                        if (y > minSize && x > minSize && currentInput[y - 1][x - 1] == '@') rollsAround++
+                        if (y > minSize && currentInput[y - 1][x] == '@') rollsAround++
+                        if (y > minSize && x < maxSize && currentInput[y - 1][x + 1] == '@') rollsAround++
+                        if (x > minSize && currentInput[y][x - 1] == '@') rollsAround++
+                        if (x < maxSize && currentInput[y][x + 1] == '@') rollsAround++
+                        if (y < maxSize && x > minSize && currentInput[y + 1][x - 1] == '@') rollsAround++
+                        if (y < maxSize && currentInput[y + 1][x] == '@') rollsAround++
+                        if (y < maxSize && x < maxSize && currentInput[y + 1][x + 1] == '@') rollsAround++
+                        if (rollsAround < 4) {
+                            count++
+                            toBeRemoved += y to x
+                        }
+                    }
+                }
+            }
+        } while (toBeRemoved.isNotEmpty())
+        return count
     }
 
     val testInput = readInput("Day04_test")
@@ -38,6 +71,6 @@ fun main() {
     test(13) { part1(testInput) }
     exec { part1(input) }
 
-    test(TODO()) { part2(testInput) }
+    test(43) { part2(testInput) }
     exec { part2(input) }
 }
