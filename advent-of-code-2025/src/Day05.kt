ddb49af245107f2f75e2e5a3669432ef8f813efb
Krystian Kaniowski <kanialdo@gmail.com>
2025-12-08 22:38:29 +0100

    aoc-2025 - Day05 - part 2

--- advent-of-code-2025/src/Day05.kt
+++ advent-of-code-2025/src/Day05.kt
@@ -1,3 +1,6 @@
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
 
     fun part1(input: List<String>): Int {
@@ -15,8 +18,36 @@ fun main() {
         }
     }
 
-    fun part2(input: List<String>): Int {
-        TODO()
+    fun part2(input: List<String>): Long {
+
+        fun LongRange.isCrossing(other: LongRange): Boolean {
+            return when {
+                this.last >= other.first -> true
+                else -> false
+            }
+        }
+
+        fun LongRange.merge(other: LongRange): LongRange {
+            return min(this.first, other.first)..max(this.last, other.last)
+        }
+
+        val middle = input.indexOf("")
+        val ranges = input.take(middle)
+            .map { it.split("-").map { it.toLong() } }
+            .map { it[0]..it[1] }
+            .sortedBy { it.first }
+
+        val optimizedRanges = ranges.fold(mutableListOf<LongRange>()) { cache, current ->
+            if (cache.lastOrNull()?.isCrossing(current) == true) {
+                val last = cache.removeLast()
+                cache.add(last.merge(current))
+            } else {
+                cache.add(current)
+            }
+            cache
+        }
+
+        return optimizedRanges.sumOf { it.last - it.first + 1 }
     }
 
     val testInput = readInput("Day05_test")
@@ -25,6 +56,6 @@ fun main() {
     test(3) { part1(testInput) }
     exec { part1(input) }
 
-    test(TODO()) { part2(testInput) }
-    exec { part2(input) }
+    testLong(14) { part2(testInput) }
+    execLong { part2(input) }
 }
